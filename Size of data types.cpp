7052213a57f3e761eb188b25b1aeec930f0c7c47
Aman Raj <160530800+rajamannn@users.noreply.github.com>
2024-11-06 00:34:25 +0530

    Add files via upload

---
+++ Size of data types.cpp
@@ -0,0 +1,17 @@
+//Program to print the size of various data types in C++
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Size of char :" <<sizeof(char) << endl;
+    cout << "Size of int :" <<sizeof(int) << endl;
+
+    cout << "Size of long :" <<sizeof(long) << endl;
+    cout << "Size of float :" <<sizeof(float) << endl;
+
+    cout << "Size of double :" <<sizeof(double) << endl;
+
+    return 0;
+}
